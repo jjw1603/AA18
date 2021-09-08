77bfc674a26494260f447da7800ead017419c963
lalalalalra <79964275+lalalalalra@users.noreply.github.com>
2021-09-08 17:13:25 +0900

    Add files via upload

---
+++ aa18-rpt02/start/hello_function.js
@@ -0,0 +1,5 @@
+function hello(what) {
+    console.log("Hello " + what + " !");
+}
+hello("aa18");
+hello("redwoods,홍길동");
---
+++ aa18-rpt02/start/index_aa18.js
@@ -0,0 +1,5 @@
+var myinfo = require('./aa18info');
+
+myinfo("aa18", "lalarala", '010-1234-4567')
+
+myinfo("aa19", "dnxo", '010-1234-5678')
