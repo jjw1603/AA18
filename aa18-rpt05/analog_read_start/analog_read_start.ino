9d2390d6055769e70dbe91e369d1401641c8e823
lalalalalra <79964275+lalalalalra@users.noreply.github.com>
2021-09-29 16:37:46 +0900

    Add files via upload

---
+++ aa18-rpt05/analog_read_start/analog_read_start.ino
@@ -0,0 +1,21 @@
+/*
+  AnalogReadSerial
+
+  Reads an analog input on pin 0, prints the result to the Serial Monitor.
+  Attach the center pin of a potentiometer to pin A0, 
+  and the outside pins to +5V and ground.
+*/
+
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+void loop() {
+int sensorValue = analogRead(A0);
+Serial.print("AA18,Present V(0~5.0): ");
+float voltage = sensorValue*(5.0/1023.0);
+Serial.println(voltage);
+delay(200);
+
+}
